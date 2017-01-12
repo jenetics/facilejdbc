d5949d288c851623c7870369d64f3cc255e08095
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2017-01-12 19:33:19 +0100

    #1: Improve LinkDAO testint.

---
+++ src/test/java/io/jenetics/jpx/jdbc/DAOTestBase.java
@@ -0,0 +1,72 @@
+/*
+ * Java GPX Library (@__identifier__@).
+ * Copyright (c) @__year__@ Franz Wilhelmstötter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Author:
+ *    Franz Wilhelmstötter (franz.wilhelmstoetter@gmail.com)
+ */
+package io.jenetics.jpx.jdbc;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+/**
+ * @author <a href="mailto:franz.wilhelmstoetter@gmail.com">Franz Wilhelmstötter</a>
+ */
+public abstract class DAOTestBase<T> {
+
+	public final DB db = H2DB.newTestInstance();
+
+	@BeforeSuite
+	public void setup() throws IOException, SQLException {
+		final String[] queries = IO.
+			toSQLText(getClass().getResourceAsStream("/model-mysql.sql"))
+			.split(";");
+
+		db.transaction(conn -> {
+			for (String query : queries) {
+				try (Statement stmt = conn.createStatement()) {
+					stmt.execute(query);
+				}
+			}
+		});
+	}
+
+	@AfterSuite
+	public void shutdown() throws SQLException {
+		db.close();
+	}
+
+	public abstract T nextObject(final Random random);
+
+	public List<T> nextObjects(final Random random) {
+		return nextObjects(random, 20);
+	}
+
+	public List<T> nextObjects(final Random random, final int count) {
+		return Stream.generate(() -> nextObject(random))
+			.limit(count)
+			.collect(Collectors.toList());
+	}
+
+}
