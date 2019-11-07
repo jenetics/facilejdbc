8e41b2371eb6b84ade518e25edca197d1d74e8f8
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2019-11-07 21:48:42 +0100

    Fix Javadoc.

--- facilejdbc/src/main/java/io/jenetics/facilejdbc/Query.java
+++ facilejdbc/src/main/java/io/jenetics/facilejdbc/Query.java
@@ -50,8 +50,6 @@ import java.util.stream.IntStream;
  * );
  * }</pre>
  *
- * @see Sql
- *
  * @author <a href="mailto:franz.wilhelmstoetter@gmail.com">Franz Wilhelmstötter</a>
  * @version !__version__!
  * @since !__version__!
