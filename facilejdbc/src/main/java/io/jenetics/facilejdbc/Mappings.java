99c3c7f86b00438a7dc66ff08c4401d237c2ccc7
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2020-12-13 22:01:59 +0100

    #43: Some code reformatting.

--- facilejdbc/src/main/java/io/jenetics/facilejdbc/Mappings.java
+++ facilejdbc/src/main/java/io/jenetics/facilejdbc/Mappings.java
@@ -28,7 +28,26 @@ final class Mappings {
 			Float.class, (Function<BigDecimal, Object>)BigDecimal::floatValue,
 			Long.class, (Function<BigDecimal, Object>)BigDecimal::longValue,
 			Integer.class, (Function<BigDecimal, Object>)BigDecimal::intValue,
-			Short.class, (Function<BigDecimal, Object>)BigDecimal::shortValue
+			Short.class, (Function<BigDecimal, Object>)BigDecimal::shortValue,
+			Byte.class, (Function<BigDecimal, Object>)BigDecimal::byteValue
+		)),
+		entry(Double.class, Map.of(
+			BigDecimal.class, (Function<Double, Object>)BigDecimal::new,
+			BigInteger.class, (Function<Double, Object>)v -> BigInteger.valueOf(v.longValue()),
+			Float.class, (Function<Double, Object>)Double::floatValue,
+			Long.class, (Function<Double, Object>)Double::longValue,
+			Integer.class, (Function<Double, Object>)Double::intValue,
+			Short.class, (Function<Double, Object>)Double::shortValue,
+			Byte.class, (Function<Double, Object>)Double::byteValue
+		)),
+		entry(Float.class, Map.of(
+			BigDecimal.class, (Function<Float, Object>)BigDecimal::new,
+			BigInteger.class, (Function<Float, Object>)v -> BigInteger.valueOf(v.longValue()),
+			Double.class, (Function<Float, Object>)Float::doubleValue,
+			Long.class, (Function<Float, Object>)Float::longValue,
+			Integer.class, (Function<Float, Object>)Float::intValue,
+			Short.class, (Function<Float, Object>)Float::shortValue,
+			Byte.class, (Function<Float, Object>)Float::byteValue
 		)),
 		entry(BigInteger.class, Map.of(
 			BigDecimal.class, (Function<BigInteger, Object>)BigDecimal::new,
@@ -36,13 +55,35 @@ final class Mappings {
 			Float.class, (Function<BigInteger, Object>)BigInteger::floatValue,
 			Long.class, (Function<BigInteger, Object>)BigInteger::longValue,
 			Integer.class, (Function<BigInteger, Object>)BigInteger::intValue,
-			Short.class, (Function<BigInteger, Object>)BigInteger::shortValue
+			Short.class, (Function<BigInteger, Object>)BigInteger::shortValue,
+			Byte.class, (Function<BigInteger, Object>)BigInteger::byteValue
 		)),
-		entry(Boolean.class, Map.of(
-			Byte.class, (Function<Boolean, Object>)v -> v ? (byte) 1 : (byte) 0,
-			Long.class, (Function<Boolean, Object>)v -> v ? (long) 1 : (long) 0,
-			Integer.class, (Function<Boolean, Object>)v -> v ? 1 : 0,
-			Short.class, (Function<Boolean, Object>)v -> v ? (short) 1 : (short) 0
+		entry(Long.class, Map.of(
+			BigDecimal.class, (Function<Long, Object>)BigDecimal::new,
+			BigInteger.class, (Function<Long, Object>)BigInteger::valueOf,
+			Double.class, (Function<Long, Object>)Long::doubleValue,
+			Float.class, (Function<Long, Object>)Long::floatValue,
+			Integer.class, (Function<Long, Object>)Long::intValue,
+			Short.class, (Function<Long, Object>)Long::shortValue,
+			Byte.class, (Function<Long, Object>)Long::byteValue
+		)),
+		entry(Integer.class, Map.of(
+			BigDecimal.class, (Function<Integer, Object>)BigDecimal::new,
+			BigInteger.class, (Function<Integer, Object>)BigInteger::valueOf,
+			Double.class, (Function<Integer, Object>)Integer::doubleValue,
+			Float.class, (Function<Integer, Object>)Integer::floatValue,
+			Long.class, (Function<Integer, Object>)Integer::longValue,
+			Short.class, (Function<Integer, Object>)Integer::shortValue,
+			Byte.class, (Function<Integer, Object>)Integer::byteValue
+		)),
+		entry(Short.class, Map.of(
+			BigDecimal.class, (Function<Short, Object>)BigDecimal::new,
+			BigInteger.class, (Function<Short, Object>)BigInteger::valueOf,
+			Double.class, (Function<Short, Object>)Short::doubleValue,
+			Float.class, (Function<Short, Object>)Short::floatValue,
+			Long.class, (Function<Short, Object>)Short::longValue,
+			Integer.class, (Function<Short, Object>)Short::intValue,
+			Byte.class, (Function<Short, Object>)Short::byteValue
 		)),
 		entry(Byte.class, Map.of(
 			BigDecimal.class, (Function<Byte, Object>)BigDecimal::valueOf,
@@ -53,50 +94,11 @@ final class Mappings {
 			Integer.class, (Function<Byte, Object>)Byte::intValue,
 			Short.class, (Function<Byte, Object>)Byte::shortValue
 		)),
-		entry(Double.class, Map.of(
-			BigDecimal.class, (Function<Double, Object>)BigDecimal::new,
-			BigInteger.class, (Function<Double, Object>)v -> BigInteger.valueOf(v.longValue()),
-			Byte.class, (Function<Double, Object>)Double::byteValue,
-			Float.class, (Function<Double, Object>)Double::floatValue,
-			Long.class, (Function<Double, Object>)Double::longValue,
-			Integer.class, (Function<Double, Object>)Double::intValue,
-			Short.class, (Function<Double, Object>)Double::shortValue
-		)),
-		entry(Float.class, Map.of(
-			BigDecimal.class, (Function<Float, Object>)BigDecimal::new,
-			BigInteger.class, (Function<Float, Object>)v -> BigInteger.valueOf(v.longValue()),
-			Byte.class, (Function<Float, Object>)Float::byteValue,
-			Double.class, (Function<Float, Object>)Float::doubleValue,
-			Long.class, (Function<Float, Object>)Float::longValue,
-			Integer.class, (Function<Float, Object>)Float::intValue,
-			Short.class, (Function<Float, Object>)Float::shortValue
-		)),
-		entry(Long.class, Map.of(
-			BigDecimal.class, (Function<Long, Object>)BigDecimal::new,
-			BigInteger.class, (Function<Long, Object>)BigInteger::valueOf,
-			Byte.class, (Function<Long, Object>)Long::byteValue,
-			Double.class, (Function<Long, Object>)Long::doubleValue,
-			Float.class, (Function<Long, Object>)Long::floatValue,
-			Integer.class, (Function<Long, Object>)Long::intValue,
-			Short.class, (Function<Long, Object>)Long::shortValue
-		)),
-		entry(Integer.class, Map.of(
-			BigDecimal.class, (Function<Integer, Object>)BigDecimal::new,
-			BigInteger.class, (Function<Integer, Object>)BigInteger::valueOf,
-			Byte.class, (Function<Integer, Object>)Integer::byteValue,
-			Double.class, (Function<Integer, Object>)Integer::doubleValue,
-			Float.class, (Function<Integer, Object>)Integer::floatValue,
-			Long.class, (Function<Integer, Object>)Integer::longValue,
-			Short.class, (Function<Integer, Object>)Integer::shortValue
-		)),
-		entry(Short.class, Map.of(
-			BigDecimal.class, (Function<Short, Object>)BigDecimal::new,
-			BigInteger.class, (Function<Short, Object>)BigInteger::valueOf,
-			Byte.class, (Function<Short, Object>)Short::byteValue,
-			Double.class, (Function<Short, Object>)Short::doubleValue,
-			Float.class, (Function<Short, Object>)Short::floatValue,
-			Long.class, (Function<Short, Object>)Short::longValue,
-			Integer.class, (Function<Short, Object>)Short::intValue
+		entry(Boolean.class, Map.of(
+			Long.class, (Function<Boolean, Object>)v -> v ? (long)1 : (long)0,
+			Integer.class, (Function<Boolean, Object>)v -> v ? 1 : 0,
+			Short.class, (Function<Boolean, Object>)v -> v ? (short)1 : (short)0,
+			Byte.class, (Function<Boolean, Object>)v -> v ? (byte)1 : (byte)0
 		)),
 
 		// Mapping of time types.
